2eb9fde7c4a349c86f2986c7abedc26af990593f
Ankita <anki333soul@gmail.com>
2021-09-01 17:29:54 -0700

    Added songs in array

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const music = document.querySelector('audio')
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+const songs = [
+    {   name: 'jacinto-4',
+        displayName: 'kun faya kun',
+        artist: 'Mohit chouhan',
+    },
+    {   name: 'jacinto-1',
+        displayName: 'Electic Chill Mechine',
+        artist: 'Jacinto design',
+    },
+    {   name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto design',
+    },
+    {   name: 'jacinto-3',
+        displayName: 'Good Night, Disco Queen',
+        artist: 'Jacinto design',
+    },
+    {   name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto design',
+    },
+];
 
 //check if playing
 let isPlaying = false;
@@ -24,3 +50,13 @@ function pauseSong() {
 playBtn.addEventListener('click', () => 
     (isPlaying ? pauseSong() : playSong())
 );
+
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+//on load
+loadSong(songs[0]);
